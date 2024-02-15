759ffacda43c8d3edc3d31c51191ff70a2fa7e81
Kolia <razunikolai@gmail.com>
2024-02-15 14:16:35 +0400

    second

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from flask import render_template, Flask, url_for
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-def index(title):
-    return render_template('base.html', name=title)
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/list_prof/<val>')
+def listProf(val):
+    list_prof = ['Инженер-исследователь', 'Инженер-строитель', 'Пилот', 'Метеоролог', 'Инженер по жизниобеспечению',
+                 'Инженер по радиоционной защите', 'Врач', 'Экзобиолог']
+    return render_template('base.html', list_prof=list_prof, val=val)
 
 
 if __name__ == '__main__':
